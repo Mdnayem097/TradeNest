8fb34da0a463342484fc5d4c222967c7e81f8266
Mdnayem097 <mdneloy097@gmail.com>
2026-09-18 20:58:56 +0600

    fix vercel deploy

---
+++ src/app/(main)/products/ProductsContent.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Suspense } from "react";
+import ProductsContent from "./ProductsContent";
+
+export default function ProductsPage() {
+  return (
+    <Suspense fallback={<div className="min-h-screen" />}>
+      <ProductsContent />
+    </Suspense>
+  );
+}
